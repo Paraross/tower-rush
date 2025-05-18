c5487734a197b9dc2f09e0baf65fe812d6ab26aa
Paraross <Adrago@onet.pl>
2025-05-18 18:11:05 +0200

    Remove unnecessary danger zone check Clean up comments

--- scripts/danger_zone.gd
+++ scripts/danger_zone.gd
@@ -1,12 +1,12 @@
-# danger_zone.gd 
-#TODO ANIMATION AND BETTER INCREASING LOGIC
+# TODO ANIMATION AND BETTER INCREASING LOGIC
 class_name DangerZone
+
 extends Area2D
 
 signal player_caught
 
-@export var initial_speed: float = 100.0  # Początkowa prędkość w pikselach/sekundę
-@export var max_speed: float = 250.0 # Maksymalna prędkość
+@export var initial_speed: float = 100.0
+@export var max_speed: float = 250.0
 
 var current_speed: float
 
@@ -21,30 +21,33 @@ func _ready() -> void:
 	# Podłącz sygnał body_entered, aby wykryć kolizję z graczem
 	body_entered.connect(_on_body_entered)
 
-func _process(delta: float) -> void:
-	# Przesuwaj DangerZone w górę
-	position.y -= current_speed * delta
 
+func _process(delta: float) -> void:
+	position.y -= current_speed * delta
+	
 	# Opcjonalnie: stopniowo zwiększaj prędkość
 	# Możesz to zrobić w _process lub np. gdy gracz osiągnie nowy poziom trudności
 	# current_speed = min(current_speed + speed_increase_rate * delta, max_speed)
 
+
 func _on_body_entered(body: Node2D) -> void:
-	if body is Player: # Upewnij się, że to gracz (użyj swojej nazwy klasy Player)
+	if body is Player:
 		print("Gracz złapany przez DangerZone!")
 		player_caught.emit()
-		# Tutaj możesz zatrzymać grę, wyświetlić ekran "Game Over" itp.
-		# Na przykład, możesz wyemitować sygnał do głównej sceny.
+		# TODO: game over screen
+
 
 func start_moving(start_position_y: float) -> void:
 	position.y = start_position_y
 	current_speed = initial_speed
 	set_process(true)
 
+
 func stop_moving() -> void:
 	set_process(false)
 
+
 func increase_difficulty_speed(new_base_speed: float) -> void:
 	# needs better logic
-	initial_speed = new_base_speed # Możesz chcieć inaczej zarządzać prędkością
+	initial_speed = new_base_speed
 	current_speed = min(initial_speed, max_speed) # Przykład bardziej złożonego skalowania
--- scripts/main.gd
+++ scripts/main.gd
@@ -45,13 +45,13 @@ func _ready() -> void:
 	for i in range(2):
 		spawn_next_platform()
 	
-	
 	# Inicjalizacja DangerZone
-	var screen_size_y: float = get_viewport().get_visible_rect().size.y # <<< POPRAWIONA LINIA
-	var initial_danger_zone_y: float = player.position.y + screen_size_y / 2 + 50 # Trochę poniżej widoku
-	danger_zone.start_moving(initial_danger_zone_y) # Przekaż pozycję startową
+	var screen_size_y := get_viewport().get_visible_rect().size.y
+	var initial_danger_zone_y := player.position.y + screen_size_y / 2 + 50
+	danger_zone.start_moving(initial_danger_zone_y)
 	danger_zone.player_caught.connect(_on_player_caught)
 	exit()
+
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel"):
@@ -63,14 +63,6 @@ func _process(_delta: float) -> void:
 	set_camera_background_positions()
 	background.set_player_position_in_shader(player.position.y)
 	handle_platform_spawning()
-	# Sprawdzanie, czy gracz nie spadł poniżej DangerZone
-	# (dodatkowe zabezpieczenie, jeśli kolizja Area2D by zawiodła lub dla natychmiastowej reakcji)
-	@warning_ignore("unsafe_property_access")
-	if player.global_position.y > danger_zone.global_position.y + (danger_zone.get_node("CollisionShape2D").shape.size.y / 2):
-		 # Dodajemy połowę wysokości DangerZone, aby upewnić się, że gracz jest faktycznie PONIŻEJ
-		if player.is_processing(): # Sprawdź, czy gra nie jest już zakończona/spauzowana
-			print("Gracz spadł poniżej DangerZone!")
-			_on_player_caught()
 
 
 func load_platform_sprites() -> void:
@@ -123,14 +115,10 @@ func spawn_next_platform() -> void:
 		danger_zone.increase_difficulty_speed(220)
 
 
-
 func _on_player_caught() -> void:
-	# Logika końca gry
 	print("GAME OVER - Player caught or fell into DangerZone")
-	exit() # Zatrzymuje przetwarzanie dla Main i Player
-	# Użyj call_deferred do przeładowania scen  
+	exit()
 	get_tree().call_deferred("reload_current_scene")
-
 
 
 func enter() -> void:
