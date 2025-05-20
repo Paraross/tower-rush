1484a102e85f7977e40efc21a22327d948af105f
Paraross <Adrago@onet.pl>
2025-05-20 13:30:00 +0200

    Change coin collision layer

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,5 +1,5 @@
+class_name Coin
 
-class_name Coin  
 extends Area2D
 
 signal coin_collected(value: int)
@@ -7,6 +7,7 @@ signal coin_collected(value: int)
 @export var coin_value: int = 10
 
 func _on_body_entered(body: Node2D) -> void:
-	if body is Player:
-		coin_collected.emit(coin_value)
-		queue_free()
+	assert(body is Player)
+	coin_collected.emit(coin_value)
+	# TODO: coin jump animation
+	queue_free()
