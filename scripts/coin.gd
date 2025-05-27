4d312d7e3dfd4b1fec44ea530cc494f7a8cbce97
Paraross <Adrago@onet.pl>
2025-05-27 17:18:30 +0200

    Add a visual effect when collecting a coin

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -6,8 +6,21 @@ signal coin_collected(value: int)
 
 @export var coin_value: int = 10
 
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var collect_effect: AnimatedSprite2D = $CollectEffect
+
 func _on_body_entered(body: Node2D) -> void:
 	assert(body is Player)
 	coin_collected.emit(coin_value)
-	# TODO: coin jump animation
+	collect_effect.show()
+	collect_effect.play("default")
+	
+	var tween := create_tween()
+	tween.set_trans(Tween.TRANS_QUAD)
+	tween.set_ease(Tween.EASE_OUT)
+	tween.tween_property(sprite, "scale", Vector2(3.0, 3.0), 0.2)
+	tween.set_ease(Tween.EASE_IN)
+	tween.tween_property(sprite, "scale", Vector2(0.0, 0.0), 0.3)
+	
+	await collect_effect.animation_finished
 	queue_free()
